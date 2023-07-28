38881451a20ccec1eeb1250cc48c3bee8b1c27a8
Anastasiia Haliara <an.haliara@gmail.com>
2023-07-28 15:38:42 +0300

    add last js-hw-11

--- js/practice.js
+++ js/practice.js
@@ -103,10 +103,10 @@ function findNarcissus(arr) {
 
 // --------------------------------------------------------------------------------------------------------//
 
-// Представлений масив фільмів (films) кожен елемент масиву являється об'єктом в якого є назва фільму та масив з жанрами до яких він відноситься, 
+// Представлений масив фільмів (films) кожен елемент масиву являється об'єктом в якого є назва фільму та масив з жанрами до яких він відноситься,
 // але в масиві(genreIDs) тільки ID цих фільмів.
-// // Потрібно в кожен об'єкт додати ключ genreNames який буде масивом з назвами жанрів до яких цей фільм відноситься. 
-// Після чого відрендерити список фільмів як наведено в прикладі нижче(Expected result), 
+// // Потрібно в кожен об'єкт додати ключ genreNames який буде масивом з назвами жанрів до яких цей фільм відноситься.
+// Після чого відрендерити список фільмів як наведено в прикладі нижче(Expected result),
 // якщо в списку жанрів більш ніж 2 фільми тоді рендеримо назву перших двох жанрів, а всі інші замінюємо на "Others".
 // Співвідношення ID жанру та назви жанру знаходиться в об'єкті genresInfo.
 
@@ -126,75 +126,126 @@ function findNarcissus(arr) {
 // // }
 
 
-// ////  Expected result
-// // Films list:
-// // 1. 'Interceptor: Action, Thriller, Others.'
-// // 2. 'Fantastic Beasts: The Secrets of Dumbledore: Fantasy, Adventure, Others.'
-// // 3. 'Last Seen Alive: Action, Thriller.'
-// // 4. 'Jurassic World Dominion: Science Fiction, Action, Others.'
+//  Expected result
+// Films list:
+// 1. 'Interceptor: Action, Thriller, Others.'
+// 2. 'Fantastic Beasts: The Secrets of Dumbledore: Fantasy, Adventure, Others.'
+// 3. 'Last Seen Alive: Action, Thriller.'
+// 4. 'Jurassic World Dominion: Science Fiction, Action, Others.'
 
-// const films = [{
-//         title: "Interceptor",
-//         genreIDs: [
-//             28,
-//             53,
-//             12,
-//         ],
-//     },
-//     {
-//         title: "Fantastic Beasts: The Secrets of Dumbledore",
-//         genreIDs: [
-//             14,
-//             12,
-//             28
-//         ],
-//     },
-//     {
-//         title: "Last Seen Alive",
-//         genreIDs: [
-//             28,
-//             53
-//         ],
-//     },
-//     {
-//         title: "Jurassic World Dominion",
-//         genreIDs: [
-//             878,
-//             28,
-//             12,
-//             53
-//         ],
-//     },
-// ];
+const films = [{
+        title: "Interceptor",
+        genreIDs: [
+            28,
+            53,
+            12,
+        ],
+    },
+    {
+        title: "Fantastic Beasts: The Secrets of Dumbledore",
+        genreIDs: [
+            14,
+            12,
+            28
+        ],
+    },
+    {
+        title: "Last Seen Alive",
+        genreIDs: [
+            28,
+            53
+        ],
+    },
+    {
+        title: "Jurassic World Dominion",
+        genreIDs: [
+            878,
+            28,
+            12,
+            53
+        ],
+    },
+];
 
+const genresInfo = {
+    genres: [{
+        id: 28,
+        name: "Action"
+    }, {
+        id: 12,
+        name: "Adventure"
+    }, {
+        id: 14,
+        name: "Fantasy"
+    }, {
+        id: 878,
+        name: "Science Fiction"
+    }, {
+        id: 53,
+        name: "Thriller"
+    }, {
+        id: 10752,
+        name: "War"
+    }, {
+        id: 37,
+        name: "Western"
+    }]
+}
 
+// //
 
-// const genresInfo = {
-//     genres: [{
-//         id: 28,
-//         name: "Action"
-//     }, {
-//         id: 12,
-//         name: "Adventure"
-//     }, {
-//         id: 14,
-//         name: "Fantasy"
-//     }, {
-//         id: 878,
-//         name: "Science Fiction"
-//     }, {
-//         id: 53,
-//         name: "Thriller"
-//     }, {
-//         id: 10752,
-//         name: "War"
-//     }, {
-//         id: 37,
-//         name: "Western"
-//     }]
+//Object example
+// {
+//     title: "Interceptor",
+//     genreIDs: [
+//         28,
+//         53,
+//         12,
+//     ],
+//     genreNames: [
+//         "Action",
+//         "Thriller",
+//         "Adventure"
+//     ]
 // }
 
-// //     .
+
+// const filmsList = document.querySelector('.js-films-list');
+
+// films.map(film => {
+//   const filmGenres = [];
+//   const genresIdsList = film.genreIDs;
+
+//   genresInfo.genres.map(genre => {
+//     if (!genresIdsList.includes(genre.id)) {
+//       return;
+//     }
+//     filmGenres.push(genre.name);
+//   })
+
+//   film.genreNames = filmGenres;
+// })
+
+// console.log("films", films);
+
+
+
+// function createMarkup(arr, list) {
+//   const markup = arr.map(({ title, genreNames }) => {
+//     if (genreNames.length <= 2) {
+//       return `<li>${title}: ${genreNames}</li>`
+//     } else {
+//       const slicesGenreNames = genreNames.slice(0, 2);
+//       return `<li>${title}: ${slicesGenreNames}, Others.</li>`
+//     }
+     
+//   }
+//    ).join('');
+
+//     list.insertAdjacentHTML('beforeend', markup);
+// }
+
+//  createMarkup(films, filmsList)
 
 
 
@@ -206,14 +257,72 @@ function findNarcissus(arr) {
 // // name приватна властивість (ім'я, успадковується від User),
 // // surname приватна властивість (прізвище, успадковується від User),
 // // year (рік вступу до вузу).
-// // Клас повинен мати метод getFullName() (успадковується від User), за допомогою якого можна вивести одночасно ім'я та прізвище студента.
-// // Також клас повинен мати метод getCourse(), який виводитиме поточний курс студента (від 1 до 5, якщо значення перевищує 5  курс виводити що студент являєтсья випускником).
-// // Курс обчислюється так: потрібно від поточного року відняти рік вступу до вузу. Поточний рік отримаєте самостійно (читати документацію!!!).
+// // Клас повинен мати метод getFullName() (успадковується від User), за допомогою якого можна вивести одночасно
+//  ім'я та прізвище студента.
+  // // Також клас повинен мати метод getCourse(), який виводитиме поточний курс студента 
+  // (від 1 до 5, якщо значення перевищує 5  курс виводити що студент являєтсья випускником).
+// // Курс обчислюється так: потрібно від поточного року відняти рік вступу до вузу. 
+// Поточний рік отримаєте самостійно(читати документацію!!!).
 // // Приклад ініціалізації екземпляру класа:
 // // const student = new Student('Петрик', 'Пяточкин', 2019);
 
 // // student.getFullName(); //поверне 'Петрик Пяточкин'
 // // student.getCourse();   //поверне 3 (третій курс)
+
+
+class User{
+  #name;
+  #surname;
+
+  constructor(name, surname) {
+    this.#name = name;
+    this.#surname = surname;
+  }
+
+  getFullName() {
+    return `${this.#name} ${this.#surname}`;
+  }
+}
+
+
+class Student extends User{
+  constructor(name, surname, year) {
+    super(name, surname);
+    this.year = year;
+  }
+
+  getEntryYear() {
+    return this.year;
+  }
+  getCourse() {
+    const delta = new Date().getFullYear() - this.year;
+    let course = null;
+    switch (delta) {
+      case 0: course = 'course: 1';
+        break;
+      case 1: course ='course: 2';
+        break;
+      case 2: course ='course: 3';
+        break;
+      case 3: course ='course: 4';
+        break;
+          
+      default: course = `${this.year + delta} year graduate`;
+
+    }
+    return course;
+  }
+}
+
+
+// const student = new Student('Петрик', 'Пяточкин', 2019);
+// console.log(student);
+// console.log(student.getFullName());
+// console.log(student.getEntryYear());
+// console.log(student.getCourse());
+
+
+
 
 //--------------------------------------- задача на чергу покупців ---------------------------------------//
 // function fn(buyers, cashNumber) {
